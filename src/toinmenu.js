b3b4fd40abcedf80fa4eaf8a102618065cf03832
juninbot99 <gtaforos03@gmail.com>
2021-02-02 04:26:35 -0300

    First commit

---
+++ src/toinmenu.js
@@ -0,0 +1,14 @@
+const toinmenu = (prefix, pushname) => {
+    return `
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃➢Comandos do Junin
+┣━━━━━━━━━━━━━━━━━━━━━━
+┃⎇${prefix}setprefix
+┃⎇${prefix}block
+┃⎇${prefix}bc
+┃⎇${prefix}bcgc
+┃⎇${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
